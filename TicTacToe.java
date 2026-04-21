def8360a91b9c1655ecf4f0e90d1fb4dbb8a4b82
KulbhushanNPatel <kulbhushanpatel9@Gmail.com>
2026-04-21 15:55:37 +0530

    UC3: Added user input method for slot selection

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,11 +1,21 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class TicTacToe {
+
+    // UC3: Method to get user input
+    public static int getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a slot number (1-9): ");
+        int slot = scanner.nextInt();
+        return slot;
+    }
+
     public static void main(String[] args) {
 
         char[][] board = new char[3][3];
 
-        // Initialize board
+        // UC1: Initialize board
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
@@ -23,7 +33,7 @@ public class TicTacToe {
 
         // UC2: Random toss
         Random random = new Random();
-        int toss = random.nextInt(2); // 0 or 1
+        int toss = random.nextInt(2);
 
         char player1Symbol, player2Symbol;
         int currentPlayer;
@@ -32,15 +42,16 @@ public class TicTacToe {
             player1Symbol = 'X';
             player2Symbol = 'O';
             currentPlayer = 1;
-            System.out.println("Player 1 starts and is assigned 'X'");
+            System.out.println("Player 1 starts and is 'X'");
         } else {
             player1Symbol = 'O';
             player2Symbol = 'X';
             currentPlayer = 2;
-            System.out.println("Player 2 starts and is assigned 'X'");
+            System.out.println("Player 2 starts and is 'X'");
         }
 
-        // Store state (just printing for now)
-        System.out.println("Current Player: Player " + currentPlayer);
+        // UC3: Take input
+        int slot = getUserInput();
+        System.out.println("You selected slot: " + slot);
     }
 }
