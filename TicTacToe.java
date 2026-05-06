d2e84fdfb27a88f4d3cd1ae14d3d64c01ed94f59
KulbhushanNPatel <kulbhushanpatel9@Gmail.com>
2026-05-06 13:03:14 +0530

    UC8: Added continuous gameplay loop with turn switching

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,9 +3,10 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    static Scanner scanner = new Scanner(System.in);
+
     // UC3
     public static int getUserInput() {
-        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a slot number (1-9): ");
         return scanner.nextInt();
     }
@@ -24,11 +25,7 @@ public class TicTacToe {
             return false;
         }
 
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
+        return board[row][col] == '-';
     }
 
     // UC6
@@ -38,15 +35,18 @@ public class TicTacToe {
 
     // Print board
     public static void printBoard(char[][] board) {
+
         for (int i = 0; i < 3; i++) {
+
             for (int j = 0; j < 3; j++) {
                 System.out.print(board[i][j] + " ");
             }
+
             System.out.println();
         }
     }
 
-    // UC7: Computer move
+    // UC7
     public static void computerMove(char[][] board, char computerSymbol) {
 
         Random random = new Random();
@@ -56,6 +56,7 @@ public class TicTacToe {
             int slot = random.nextInt(9) + 1;
 
             int[] pos = getRowCol(slot);
+
             int row = pos[0];
             int col = pos[1];
 
@@ -64,58 +65,92 @@ public class TicTacToe {
                 updateBoard(board, row, col, computerSymbol);
 
                 System.out.println("Computer selected slot: " + slot);
+
                 break;
             }
         }
+    }
+
+    // UC8: Check draw
+    public static boolean isBoardFull(char[][] board) {
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
 
         char[][] board = new char[3][3];
 
-        // UC1 Initialize board
+        // Initialize board
         for (int i = 0; i < 3; i++) {
+
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
             }
         }
 
-        System.out.println("Initial Board:");
-        printBoard(board);
-
-        // UC2 Toss
-        Random random = new Random();
-        int toss = random.nextInt(2);
-
-        int currentPlayer = (toss == 0) ? 1 : 2;
-
         char humanSymbol = 'X';
         char computerSymbol = 'O';
 
-        System.out.println("Player " + currentPlayer + " starts");
+        boolean humanTurn = true;
+        boolean gameRunning = true;
 
-        // Human move
-        int slot = getUserInput();
-
-        int[] pos = getRowCol(slot);
-        int row = pos[0];
-        int col = pos[1];
-
-        if (isValidMove(board, row, col)) {
-
-            updateBoard(board, row, col, humanSymbol);
-
-            System.out.println("Board after Human move:");
-            printBoard(board);
-
-        } else {
-            System.out.println("Invalid move ");
-        }
-
-        // UC7 Computer move
-        computerMove(board, computerSymbol);
-
-        System.out.println("Board after Computer move:");
+        System.out.println("Initial Board:");
         printBoard(board);
+
+        // UC8 Game Loop
+        while (gameRunning) {
+
+            if (humanTurn) {
+
+                int slot = getUserInput();
+
+                int[] pos = getRowCol(slot);
+
+                int row = pos[0];
+                int col = pos[1];
+
+                if (isValidMove(board, row, col)) {
+
+                    updateBoard(board, row, col, humanSymbol);
+
+                    System.out.println("Human Move:");
+                    printBoard(board);
+
+                    humanTurn = false;
+
+                } else {
+
+                    System.out.println("Invalid move ");
+                }
+
+            } else {
+
+                computerMove(board, computerSymbol);
+
+                System.out.println("Computer Move:");
+                printBoard(board);
+
+                humanTurn = true;
+            }
+
+            // Stop if board full
+            if (isBoardFull(board)) {
+
+                System.out.println("Game Draw!");
+
+                gameRunning = false;
+            }
+        }
     }
 }
